92b32bf0ff5070ee36a07e20c8070ee8d5ca3264
guliang21 <gl_1573@163.com>
2019-09-02 12:57:18 +0800

    protobuf C# demo

---
+++ CSharp/Protobuf/Program.cs
@@ -0,0 +1,33 @@
+﻿using Google.Protobuf;
+using System;
+using Test;
+
+namespace Protobuf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "张三";
+            person.Age = 20;
+            person.Marriage = true;
+
+            // 序列化
+            byte[] buffer = person.ToByteArray();
+
+            foreach (byte b in buffer)
+            {
+                Console.Write(b.ToString("X2") + " ");
+            }
+            Console.WriteLine();
+
+            // 反序列化
+            Person p = Person.Parser.ParseFrom(buffer);
+
+            Console.WriteLine(string.Format("Name: {0}, Age: {1}, Marriage: {2}", p.Name, p.Age, p.Marriage));
+
+            Console.Read();
+        }
+    }
+}
