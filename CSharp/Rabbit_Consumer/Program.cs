372386279a8a8f042dd3379ee645a4da9db9cc25
guliang21 <gl_1573@163.com>
2019-09-02 11:21:30 +0800

    RabbitMQ C# demo

---
+++ CSharp/Rabbit_Consumer/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string host = "localhost";
+            string queue_name = "hello";
+
+            var factory = new ConnectionFactory() { HostName = host };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: queue_name, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("Received：{0}", message);
+                };
+                channel.BasicConsume(queue: queue_name, autoAck: true, consumer: consumer);
+
+                Console.WriteLine("Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ CSharp/Rabbit_Producer/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQ_Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string host = "localhost";
+            string queue_name = "hello";
+            string routing_key = "hello";
+
+            ConnectionFactory factory = new ConnectionFactory() { HostName = host };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: queue_name, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                    string message = "Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "", routingKey: routing_key, basicProperties: null, body: body);
+                    Console.WriteLine("Sent：{0}", message);
+                }
+            }
+
+            Console.WriteLine("Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
